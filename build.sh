08d6f513f4fc9f2ee0b7bd573e5c30cbaca133a2
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-06-26 10:00:35 +0200

    build → buildx build  –  multiarch amd64/arm64 etc.

--- build.sh
+++ build.sh
@@ -3,4 +3,17 @@
 set -ex
 
 export DOCKER_BUILDKIT=1
-docker build --pull --progress=plain --tag pklaus/mvd_epics:1-0-0 .
+export BUILDKIT_PROGRESS=auto # auto plain ...
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# if you want to start with a clean builder, run:
+#docker buildx rm epics_builder || true
+
+docker buildx inspect epics_builder || \
+  docker buildx create --name epics_builder
+docker buildx use epics_builder
+docker buildx inspect --bootstrap epics_builder
+
+docker buildx build --pull --push --platform linux/amd64,linux/386,linux/arm/v7,linux/arm64/v8 --tag pklaus/mvd_epics:1-0-0 .
